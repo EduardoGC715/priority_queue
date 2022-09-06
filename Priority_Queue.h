a1b75d5100b7696fd43c2825c08af81402a40c2b
EduardoGC715 <99993140+EduardoGC715@users.noreply.github.com>
2022-09-06 14:45:49 -0600

    Update Priority_Queue.h

--- Priority_Queue.h
+++ Priority_Queue.h
@@ -42,7 +42,7 @@ public:
                 this->last->setPrevious(newNode);
                 this->last = newNode;
             }
-        } 
+        }
         else {
             this->first = newNode;
             this->last = newNode;
